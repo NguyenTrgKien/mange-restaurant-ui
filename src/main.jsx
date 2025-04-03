0ee2d6c77ae264d352dc150ba0218755a6d95e62
TRUNGKIEN <nguyentrungkien040921@gmail.com>
2025-04-03 16:11:09 +0700

    update router

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import '../src/assets/css/style.css';
 import {GoogleOAuthProvider} from '@react-oauth/google';
-import { BrowserRouter as Router } from 'react-router';
+import { HashRouter as Router } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './redux/store.js';
 import { FoodProvider } from './contexts/FoodContext.jsx';
